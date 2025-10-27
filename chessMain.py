6bb4263bf6522de0ef760f6bb2fd9712aa28edb3
Keshav Mishra <keshavmishra2112@gmail.com>
2025-10-27 13:41:02 +0530

    Implement piece movement

--- chessMain.py
+++ chessMain.py
@@ -33,10 +33,28 @@ def main():
     print(gs.board)
     loadImages()  # only do this once, before the while loop
     running = True
+    sqSelected = ()  # no square is selected, keep track of the last click of the user (tuple: (row,col))
+    playerClicks = []  # keep track of player clicks (two tuples: [(6,4), (4,4)])
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # (x,y) location of mouse
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col):  # the user clicked the same square twice
+                    sqSelected = ()  # deselect
+                    playerClicks = []  # clear player clicks
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)  # append for both 1st and 2nd clicks
+                if len(playerClicks) == 2:  # after 2nd click
+                    move = chessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()  # reset user clicks
+                    playerClicks = []
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
